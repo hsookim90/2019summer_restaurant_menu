d1ed4e036c113b8fb2278ad918f731deda2c8adc
Harrison <hsookim90@gmail.com>
2019-05-10 19:34:18 -0500

    add google maps

---
+++ script.js
@@ -0,0 +1,36 @@
+function openTab(event, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+
+    loadData(tabName);
+}
+
+// function loadData(tabName) {
+//     $(document).ready(function() {
+//         $("#" + tabName).click(function() {
+//             $()
+//         })
+//     });
+// }
+
+function initMap() {
+    var options = {
+        zoom: 8,
+        center:{lat:49.8951, lng:97.1384} 
+    }
+
+    var map = new google.maps.Map(document.getElementById('map'), options);
+}
+
+document.getElementById("defaultOpen").click();
