148cf7a44fd14353ab61b91e8fd9692b304ffec4
Harrison <hsookim90@icloud.com>
2019-07-12 14:28:42 -0500

    sticky header bug fix

--- public/js/home.js
+++ public/js/home.js
@@ -23,18 +23,21 @@ function closeNav() {
 }
 
 // --------------- Scroll
-window.onscroll = function() {myFunction()};
+document.addEventListener('DOMContentLoaded', function(event) {
+    window.onscroll = function() {myFunction()};
+    
+    var header = document.getElementById("myHeader");
+    var sticky = header.offsetTop;
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
     }
-}
+})
+
 
 // ------------------------------------------------------ //
 // Google Maps
--- public/main/list.php
+++ public/main/list.php
@@ -37,20 +37,6 @@
         </div>
     </div>
 
-    <script>
-        window.onscroll = function() {myFunction()};
-
-        var header = document.getElementById("myHeader");
-        var sticky = header.offsetTop;
-
-        function myFunction() {
-            if (window.pageYOffset > sticky) {
-                header.classList.add("sticky");
-            } else {
-                header.classList.remove("sticky");
-            }
-        }
-    </script>
 
     <?php require 'footer.php';?>
 </body>
